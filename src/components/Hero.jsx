4c89594e7ecf5076128e911e0246e465b5452a75
l0eb <mrloebdesanta@gmail.com>
2024-02-19 22:44:08 +0530

    newsc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       </nav>
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden"/>
-        <span className="orange_gradient">Open AI GPT-4</span>
+        <span className="orange_gradient">Open AI GPT-3.5</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries
